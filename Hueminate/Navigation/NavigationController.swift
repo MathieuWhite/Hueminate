ea1d1cd7f5ae110b42e7952c05cca888a152bfae
MathieuWhite <mathieuwhite91@gmail.com>
2016-04-30 07:20:20 -0400

    - Initial project setup

---
+++ Hueminate/Navigation/NavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  NavigationController.swift
+//  Lights
+//
+//  Created by Mathieu White on 2016-04-29.
+//  Copyright © 2016 Mathieu White. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
